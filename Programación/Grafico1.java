8d330c0e8045c05c509880bafe24db8a93bffa81
1KATZUNO <147660938+1KATZUNO@users.noreply.github.com>
2024-05-04 00:19:54 -0600

    njk

--- Programación/Grafico1.java
+++ Programación/Grafico1.java
@@ -1,7 +1,14 @@
 package Programación;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 public class Grafico1 extends JFrame{
-    
+   
+
+
+public void Inicializar(){
+
+  
+
 }
